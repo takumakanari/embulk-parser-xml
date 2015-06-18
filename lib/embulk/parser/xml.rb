3f4640696bb416f70b848bc8529f584996b42ad0
takumakanari <chemtrails.t@gmail.com>
2015-06-18 14:44:44 +0900

    extra schema, draft

--- lib/embulk/parser/xml.rb
+++ lib/embulk/parser/xml.rb
@@ -8,12 +8,18 @@ module Embulk
 
       def self.transaction(config, &control)
         schema = config.param("schema", :array)
+        extra_schema = config.param("extra_schema", :array, default: [])
         schema_serialized = schema.inject({}) do |memo, s|
           memo[s["name"]] = s["type"]
           memo
         end
+        extra_schema_serialized = extra_schema.inject({}) do |memo, s|
+          memo[s["path"]] = {:name => s["name"], :type => s["type"]}
+          memo
+        end
         task = {
           :schema => schema_serialized,
+          :extra_schema => extra_schema_serialized,
           :root_to_route => config.param("root", :string).split("/")
         }
         columns = schema.each_with_index.map do |c, i|
@@ -26,8 +32,8 @@ module Embulk
         on_new_record = lambda {|record|
           @page_builder.add(record)
         }
-        doc = RecordBinder.new(@task["root_to_route"],
-                               @task["schema"], on_new_record)
+        doc = RecordBinder.new(@task["root_to_route"], @task["schema"],
+                               @task["extra_schema"], on_new_record)
         parser = Nokogiri::XML::SAX::Parser.new(doc)
         while file = file_input.next_file
           parser.parse(file.read)
@@ -39,10 +45,12 @@ module Embulk
 
     class RecordBinder  < Nokogiri::XML::SAX::Document
 
-      def initialize(route, schema, on_new_record)
+      def initialize(route, schema, extra_schema, on_new_record)
         @route = route
         @schema = schema
+        @extra_schema = extra_schema
         @on_new_record = on_new_record
+        @element_path = ""
         clear
         super()
       end
@@ -52,6 +60,8 @@ module Embulk
         @enter = false
         @current_element_name = nil
         @current_data = new_map_by_schema
+        @extra_data = {} # TODO init by method
+        @element_path = ""
       end
 
       def start_element(name, attributes = [])
@@ -63,17 +73,17 @@ module Embulk
               @find_route_idx += 1
             end
           end
-        else
-          @current_element_name = (@schema[name].nil?) ? nil : name
         end
+        @current_element_name  = name
+        @current_element_name = name
+        @element_path += "/" if @element_path.size > 0
+        @element_path += "#{name}"
       end
 
       def characters(string)
-        return if !@enter || string.strip.size == 0 || @current_element_name.nil?
-        val = @current_data[@current_element_name]
-        val = "" if val.nil?
-        val += string
-        @current_data[@current_element_name] = val
+        return if string.strip.size == 0
+        @current_data[@current_element_name] ||= ""
+        @current_data[@current_element_name] += string
       end
 
       def end_element(name, attributes = [])
@@ -82,13 +92,26 @@ module Embulk
             @enter = false
             @on_new_record.call(@current_data.map{|k, v| v})
             @current_data = new_map_by_schema
-          elsif !@current_element_name.nil? && @schema.key?(name)
+          elsif @schema.key?(name)
             @current_data[name] = convert(@current_data[name], @schema[name])
           end
         end
+        if @extra_schema && @extra_schema.key?(@element_path)
+          # FIXME re-arrange data handle ...
+          @extra_data[name] = convert(@current_data[name],
+                                      @extra_schema[@element_path]["type"])
+        end
+        @current_element_name = nil
+        pop_element_path
       end
 
       private
+
+      def pop_element_path
+        path_list = @element_path.split("/")
+        path_list.delete_at(-1)
+        @element_path = path_list.join("/")
+      end
 
       def new_map_by_schema
         @schema.keys.inject({}) do |memo, k|
